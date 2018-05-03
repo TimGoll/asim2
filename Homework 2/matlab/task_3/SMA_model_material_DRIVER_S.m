6d837b96db75785eb00a19087bbf2a344459f107
TimDerGoll <tim@timgoll.de>
2018-05-03 17:35:01 +0200

    assignment 2 done
    interpretation todo

---
+++ Homework 2/matlab/task_3/SMA_model_material_DRIVER_S.m
@@ -0,0 +1,112 @@
+
+% clear;
+% close all;
+global E_A E_M epsilon_T V_d tau_x T0 epsilon_A_T0 epsilon_M_T0 dsigma_dT
+global k_b f_stress f_temperature
+
+
+% ---------------------------------------------------------------------
+%
+% input information
+%
+% ---------------------------------------------------------------------
+
+E_A = 70.0e9;			%	Austenite modulus of elasticity		[Pa]
+E_M = 30.0e9;			%	Martensite modulus of elasticity	[Pa]
+epsilon_T = 0.045;		%	epsilon_T			[-]
+epsilon_A_T0 = 0.006;   %   epsilon_A at temperature T0 [-]
+epsilon_M_T0 = 0.05;    %   epsilon_A at temperature T0 [-]
+dsigma_dT = 7e6;        %   dsigma/dtemp [Pa]
+T0 = 323;               %	reference temperature at which epsilon_A_T0 and epsilon_M_T0 are measured  [K]
+V_d = 1e-23;            % 	volume of a layer	[m^3]
+tau_x = 1e-2;			%	vibration frequency	[1/s]
+k_b = 1.38044e-23;	    %	Boltzman constant
+  
+%%%%%% CHANGED HERE %%%%%%
+f_stress = @(t) 0.5*10^9;  %   Stress function over time t [Pa]
+f_temperature = @(t) 293 + 30 * ( sin(2*pi*t/10) + 1 );    %   Temperature function over time t
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+tspan=[0,30];
+y_initial=[0,0];
+
+options = odeset('RelTol',1e-6,'AbsTol',1e-6);
+
+
+% initialize cputime measurement for ODE solver
+tt=cputime;
+
+
+[t,y] = ode15s('SMA_model_material_S',tspan,y_initial,options);
+
+% elapsed cputime
+e = cputime - tt
+
+stress = t*0;
+temp = t*0;
+
+for i = 1:length(t)
+    stress(i) = f_stress(t(i));
+    temp(i) = f_temperature(t(i));
+end
+
+if length(stress) == 1
+    stress = stress*ones(size(t));
+end
+if length(temp) == 1
+    temp = temp*ones(size(t));
+end
+
+%%%%% Stress value %%%%%%%%
+
+xpls = y(:,1);
+xmns = y(:,2);
+xaus = 1-xpls-xmns;
+
+strain = stress.*( (xpls + xmns)/E_M  + (xaus)/E_A )+ xpls.*epsilon_T - xmns.*epsilon_T;
+
+%***********************%%%%%%%%%%
+
+
+
+subplot(2,3,1),plot(t,strain,'LineWidth',2);
+set(gca,'FontSize',14,'Fontname','Arial');
+xlabel('Time [s]','FontName','Arial','FontSize',14);
+ylabel('Strain [-]','FontName','Arial','FontSize',14);
+xlim([min(t) max(t)]);
+grid on
+
+subplot(2,3,2),plot(t,xpls,t,xmns,t,xaus,'LineWidth',2);
+set(gca,'FontSize',14,'Fontname','Arial');
+xlabel('Time [s]','FontName','Arial','FontSize',14);
+ylabel('Phase Fractions [-]','FontName','Arial','FontSize',14);
+xlim([min(t) max(t)]);
+ylim([-0.2 1.2]);
+grid on
+legend('x_+','x_-','x_A','Location','NE');
+
+subplot(2,3,3),plot(t,temp,'LineWidth',2);
+set(gca,'FontSize',14,'Fontname','Arial');
+xlabel('Time [s]','FontName','Arial','FontSize',14);
+ylabel('Temperature [K]','FontName','Arial','FontSize',14);
+xlim([min(t) max(t)]);
+grid on
+
+subplot(2,3,4),plot(t,stress*10^-9,'LineWidth',2);
+set(gca,'FontSize',14,'Fontname','Arial');
+xlabel('Time [s]','FontName','Arial','FontSize',14);
+ylabel('Stress [GPa]','FontName','Arial','FontSize',14);
+xlim([min(t) max(t)]);
+grid on
+
+subplot(2,3,5),plot(strain,stress*10^-9,'LineWidth',2);
+set(gca,'FontSize',14,'Fontname','Arial');
+xlabel('Strain [-]','FontName','Arial','FontSize',14);
+ylabel('Stress [GPa]','FontName','Arial','FontSize',14);
+grid on
+
+subplot(2,3,6),plot(temp,strain,'LineWidth',2);
+set(gca,'FontSize',14,'Fontname','Arial');
+xlabel('Temperature [K]','FontName','Arial','FontSize',14);
+ylabel('Strain [-]','FontName','Arial','FontSize',14);
+grid on
---
+++ Homework 2/matlab/task_3/SMA_model_material_E.m
@@ -0,0 +1,116 @@
+function [f] = SMA_model_material_E(t, var)
+
+global E_A E_M epsilon_T V_d tau_x T0 epsilon_A_T0 epsilon_M_T0 dsigma_dT
+global k_b f_strain f_temperature
+
+xpls = var(1);
+xmns = var(2);
+xaus = 1 - xpls - xmns;
+
+%****** Inputs *********
+epsilon = f_strain(t);
+temp = f_temperature(t);
+
+%%**********************
+
+% Compute here the stress
+
+sigma = (epsilon - epsilon_T*(xpls - xmns))/(xaus/E_A+(xpls+xmns)/E_M); 
+
+% Compute model coefficients at temperature T
+
+sigma_A_T0 = E_A*epsilon_A_T0;
+sigma_M_T0 = E_M*(epsilon_M_T0 - epsilon_T);
+
+sigma_A_T = sigma_A_T0 + dsigma_dT*(temp - T0);
+sigma_M_T = sigma_M_T0 + dsigma_dT*(temp - T0);
+
+epsilon_A_T = sigma_A_T/E_A;
+epsilon_M_T = sigma_M_T/E_M + epsilon_T;
+
+beta_T_old = 0.5*(E_M*(epsilon_T-epsilon_A_T)*(epsilon_T-epsilon_M_T)-E_A*epsilon_A_T*epsilon_M_T);
+
+if sigma_A_T < 0  % Be sure that sigma_A is always positive
+    sigma_A_T = 0;
+    sigma_M_T = sigma_A_T - (sigma_A_T0 - sigma_M_T0);
+    epsilon_A_T = sigma_A_T/E_A;
+    epsilon_M_T = sigma_M_T/E_M + epsilon_T;
+    beta_T = 0.5*(E_M*(epsilon_T-epsilon_A_T)*(epsilon_T-epsilon_M_T)-E_A*epsilon_A_T*epsilon_M_T);
+else
+    beta_T = beta_T_old; 
+end
+
+% Compute strain values associated with each of energy well extremum:
+
+a(1) = E_M/2;
+a(2) = (E_M*(epsilon_T-epsilon_M_T)+E_A*epsilon_A_T)/2/(epsilon_A_T-epsilon_M_T);
+a(3) = E_A/2;
+a(4) = a(2);
+a(5) = a(1);
+
+b(1) = E_M*epsilon_T;
+b(2) = epsilon_A_T*(E_M*(epsilon_T-epsilon_M_T)+E_A*epsilon_M_T)/(epsilon_A_T-epsilon_M_T);
+b(3) = 0;
+b(4) = -b(2);
+b(5) = -E_M*epsilon_T;
+
+c(1) = E_M/2*epsilon_T^2;
+c(2) = (2*beta_T*(epsilon_A_T-epsilon_M_T)+epsilon_A_T^2*(E_M*(epsilon_T-epsilon_M_T)+E_A*epsilon_M_T))/2/(epsilon_A_T-epsilon_M_T);
+c(3) = beta_T;
+c(4) = c(2);
+c(5) = c(1);
+
+eps_ext = (sigma - b)./a/2;
+
+if eps_ext(1) > -epsilon_M_T
+    eps_ext(1)=-epsilon_M_T;
+end
+if eps_ext(2) < -epsilon_M_T
+    eps_ext(2)=-epsilon_M_T;
+elseif eps_ext(2) > -epsilon_A_T
+    eps_ext(2)=-epsilon_A_T;
+end
+
+if eps_ext(3) < -epsilon_A_T
+    eps_ext(3)=-epsilon_A_T;
+elseif eps_ext(3) > epsilon_A_T
+    eps_ext(3)=epsilon_A_T;
+end
+
+if eps_ext(4) < epsilon_A_T
+    eps_ext(4)=epsilon_A_T;
+elseif eps_ext(4) > epsilon_M_T
+    eps_ext(4)=epsilon_M_T;
+end
+
+if eps_ext(5) < epsilon_M_T
+    eps_ext(5)=epsilon_M_T;
+end
+
+
+G = a.*eps_ext.^2 + b.*eps_ext + c - sigma*eps_ext;
+
+
+deltaG_mA = G(2)-G(1);
+deltaG_Am = G(2)-G(3);
+deltaG_Ap = G(4)-G(3);
+deltaG_pA = G(4)-G(5);
+
+lambda = V_d/k_b/temp;
+
+
+pma = 1/tau_x*exp(-lambda*deltaG_mA);
+pam = 1/tau_x*exp(-lambda*deltaG_Am);
+ppa = 1/tau_x*exp(-lambda*deltaG_pA);
+pap = 1/tau_x*exp(-lambda*deltaG_Ap);
+
+
+% ------------------------------------------------------------------------
+%
+%	subroutine FCN	 --	RHS of ODE system
+%
+% ------------------------------------------------------------------------
+
+f(1) = -xpls*ppa + xaus*pap;
+f(2) = -xmns*pma + xaus*pam;
+f = [f(1);f(2)];
