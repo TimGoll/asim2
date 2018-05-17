02c5fe682111ce4e9e4dbdde501d7dda9ab9fcca
Matthias Jost <s9msjost@stud.uni-saarland.de>
2018-05-17 17:16:53 +0200

    Aufgabe 2

---
+++ Homework 3/Plot_A_2.m
@@ -0,0 +1,10 @@
+%frequency = 0.01;
+%a = sim('SMA_displacementIn_edit','SimulationMode','normal');
+
+figure('Name', 'SMA mass actuator');
+plot(time, force, 'Linewidth', 2);
+grid;
+grid minor;
+xlabel 'Time [s]';
+ylabel 'Force [N]';
+set(gca, 'FontSize', 14);
