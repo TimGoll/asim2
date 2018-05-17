af283816e64b319ac905f3c55fc7f55d81e7356c
TimDerGoll <tim@timgoll.de>
2018-05-17 17:15:44 +0200

    task1 done

--- Homework 3/task_1.m
+++ Homework 3/task_1.m
@@ -1,10 +1,25 @@
-frequency = 0.01;
+frequency_arr = [0.01 0.04 0.1 0.25 1]; %Hz
+
+J = 0.2;
+
+for i = 1:1:length(frequency_arr)
+    disp(i);
+    frequency = frequency_arr(i);
+    plot__(frequency_arr(i));
+end
+
+function [] = plot__ (frequency) 
+
 a = sim('SMA_displacementIn_edit','SimulationMode','normal');
 
-figure('Name', 'SMA displacement [0.01Hz]');
-plot(a.force, a.displacement, 'Linewidth', 1);
+figure('Name', 'SMA displacement [' + string(frequency) + 'Hz]');
+plot(a.get('displacement'), a.get('force'), 'Linewidth', 2);
 grid;
 grid minor;
-xlabel 'Force [N]';
-ylabel 'Displacement';
+xlabel 'Displacement [m]';
+ylabel 'Force [N]';
 set(gca, 'FontSize', 14);
+xlim([0.1 0.107]);
+ylim([0 4.5]);
+
+end
