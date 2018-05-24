e24217c13cac9e7c39a9381c37ef2fac708f1fd4
TimDerGoll <tim@timgoll.de>
2018-05-24 13:17:57 +0200

    fixed assignment03

--- Homework 3/Plot_A_2.m
+++ Homework 3/Plot_A_2.m
@@ -2,12 +2,12 @@
 %a = sim('SMA_displacementIn_edit','SimulationMode','normal');
 
 figure('Name', 'SMA mass actuator');
-%plot(time, force, 'Linewidth', 2);
-plot(time, displacement, 'Linewidth', 2);
+plot(time, force, 'Linewidth', 2);
+%plot(time, displacement, 'Linewidth', 2);
 
 grid;
 grid minor;
 xlabel 'Time [s]';
-%ylabel 'Force [N]';
-ylabel 'Displacement [m]';
+ylabel 'Force [N]';
+%ylabel 'Displacement [m]';
 set(gca, 'FontSize', 14);
