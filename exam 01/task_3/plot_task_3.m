c20addbfa0f15eb1633f1eb382e9d6a8af7cf80c
Matthias Jost <s9msjost@stud.uni-saarland.de>
2018-06-12 15:25:42 +0200

    task 4

--- exam 01/task_3/plot_task_3.m
+++ exam 01/task_3/plot_task_3.m
@@ -9,7 +9,8 @@ param_preload          = 0.05;
 param_m                = 0.3;
 param_b                = 10;
 param_k                = 500;
-params_PC2.delta_T = 80;
+params_PC2.delta_T     = 80;
+param_J0_yin           = 0.7845;
 
 param_J = params_PC2.alpha * 2 * pi * params_PC2.r0 * params_PC2.L0 * param_N * params_PC2.delta_T;
 
@@ -19,9 +20,9 @@ a = sim('SMA_displacementIn_PC2','SimulationMode','normal');
 %% PLOT
 figure('Name', 'SMA actuator [Force-Displacement SMA]');
 
-plot(a.get('time'), a.get('temperature'), 'Linewidth', 2);
+%plot(a.get('time'), a.get('temperature'), 'Linewidth', 2);
 
-hold on;
+%hold on;
 plot(a.get('time'), a.get('normalized_stroke'), 'Linewidth', 2);
 
 
