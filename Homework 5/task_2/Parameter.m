fc16f5b3503b509ca7d71a5a00852affbf6d1f30
TimDerGoll <tim@timgoll.de>
2018-06-28 17:21:26 +0200

    finishing up task2 0V

---
+++ Homework 5/task_2/Parameter.m
@@ -0,0 +1,22 @@
+% parameter list
+
+
+params = struct;		% system parameters
+
+params.L3 = 40e-6;             %	DE membrane 3-length, thickness		[m]
+params.epsilon_0 = 8.854e-12;  % 	Void permittivity			[F/m]
+%params.epsilon_r = 3;		   %	DE relative permittivity
+params.epsilon_r = 4;		   %	DE relative permittivity
+%params.C1 = 1e5;               %	DE Neo Hookean model coefficient [Pa]
+params.C1 = 2.7e5;               %	DE Neo Hookean model coefficient [Pa]
+params.kv = 3e5;               % 	DE viscoelastic model stiffness	[Pa]
+params.bv = 1e5;               %    DE viscoelastic model damping [Pa*s]
+
+
+
+initConds = struct;		% initial conditions
+initConds.epsilon_v 		= 0*ones(size(params.kv));                	% 	Initial viscoelastic strain
+
+
+
+
