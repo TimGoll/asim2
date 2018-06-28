040fecc6374f849e0601d20f132a267c6e885da0
TimDerGoll <tim@timgoll.de>
2018-06-28 18:09:30 +0200

    finished task3

--- Homework 5/task_3/Parameter.m
+++ Homework 5/task_3/Parameter.m
@@ -5,10 +5,12 @@ params = struct;		% system parameters
 
 params.L3 = 40e-6;             %	DE membrane 3-length, thickness		[m]
 params.epsilon_0 = 8.854e-12;  % 	Void permittivity			[F/m]
-params.epsilon_r = 3;		   %	DE relative permittivity
-params.C1 = 1e5;               %	DE Neo Hookean model coefficient [Pa]
-params.kv = 1e5;               % 	DE viscoelastic model stiffness	[Pa]
-params.bv = 1e5;               %    DE viscoelastic model damping [Pa*s]
+%params.epsilon_r = 3;		   %	DE relative permittivity
+params.epsilon_r = 4;		   %	DE relative permittivity
+%params.C1 = 1e5;               %	DE Neo Hookean model coefficient [Pa]
+params.C1 = 2.73e5;               %	DE Neo Hookean model coefficient [Pa]
+params.kv = 1.7e5;               % 	DE viscoelastic model stiffness	[Pa]
+params.bv = 0.75e5;               %    DE viscoelastic model damping [Pa*s]
 
 
 
---
+++ Homework 5/task_3/task_3.m
@@ -0,0 +1,38 @@
+close ALL;
+clc;
+clear;
+
+[time, strain, stress] = read_and_parse('DE_relaxation_0V');
+
+Parameter;
+
+params_voltage = 0;
+param_f = 1;
+
+a = sim('DE_epsIn_task_3', 'SimulationMode', 'normal');
+
+figure('Name', 'SMA actuator [Time-Stress]');
+plot(stress, strain, 'Linewidth', 2, 'DisplayName', 'measured');
+hold on;
+plot(a.get('stress'), a.get('viscoelastic_strain'), 'Linewidth', 2, 'DisplayName', 'simulated');
+grid;
+grid minor;
+xlabel 'Stress [Pa]';
+ylabel 'Strain [%]';
+set(gca, 'FontSize', 14);
+legend('show')
+
+
+
+
+
+function [time, strain, stress] = read_and_parse(filepath)
+
+fileID = fopen(filepath, 'r');
+A = fscanf(fileID, '%f', [3, Inf]);
+
+time   = A(1,:);
+strain = A(2,:);
+stress = A(3,:);
+
+end
