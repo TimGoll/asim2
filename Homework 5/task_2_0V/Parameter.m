cc8b535d2dc8d06c1778ce8eb3e4e20ce2781a6b
TimDerGoll <tim@timgoll.de>
2018-06-28 17:35:05 +0200

    splitted

--- Homework 5/task_2_0V/Parameter.m
+++ Homework 5/task_2_0V/Parameter.m
@@ -9,7 +9,7 @@ params.epsilon_0 = 8.854e-12;  % 	Void permittivity			[F/m]
 params.epsilon_r = 4;		   %	DE relative permittivity
 %params.C1 = 1e5;               %	DE Neo Hookean model coefficient [Pa]
 params.C1 = 2.7e5;               %	DE Neo Hookean model coefficient [Pa]
-params.kv = 3e5;               % 	DE viscoelastic model stiffness	[Pa]
+params.kv = 1e5;               % 	DE viscoelastic model stiffness	[Pa]
 params.bv = 1e5;               %    DE viscoelastic model damping [Pa*s]
 
 
