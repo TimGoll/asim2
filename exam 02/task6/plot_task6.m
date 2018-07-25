9ec75b66b66ba54ede1b16b1eb0320c8203e1c82
Tim Goll <tim@timgoll.de>
2018-07-25 16:02:18 +0200

    updated task6

--- exam 02/task6/plot_task6.m
+++ exam 02/task6/plot_task6.m
@@ -67,21 +67,21 @@ xlim([0 param_simtime]);
 ylim([-0.002 0.002]);
 
 figure('Name', 'f=34.5 // pwm - POWER');
-plot(a1.get('time'), a1.get('power'), 'Linewidth', 2, 'DisplayName', 'y_2');
+plot(a1.get('time'), a1.get('power') ./ a1.get('time'), 'Linewidth', 2, 'DisplayName', 'y_2');
 grid;
 grid minor;
 ylabel 'Power [W]';
 xlabel 'Time [s]';
 set(gca, 'FontSize', 20);
 xlim([0 param_simtime]);
-%ylim([-10 10]);
+ylim([-1 10]);
 
 power_ = a1.get('power');
-power_result = 0;
-for i=1:1:size(power_)
-    power_result = power_result + power_(i);
-end
-power_result = power_result / param_simtime;
+%power_result = 0;
+%for i=1:1:size(power_)
+%    power_result = power_result + power_(i);
+%end
+power_result = power_(end) / param_simtime;
 disp('f=34.5 // pwm - POWER: ' + string(power_result));
 
 
@@ -107,21 +107,21 @@ xlim([0 param_simtime]);
 ylim([-0.002 0.002]);
 
 figure('Name', 'f=34.5 // sawtooth - POWER');
-plot(b1.get('time'), b1.get('power'), 'Linewidth', 2, 'DisplayName', 'y_2');
+plot(b1.get('time'), b1.get('power') ./ b1.get('time'), 'Linewidth', 2, 'DisplayName', 'y_2');
 grid;
 grid minor;
 ylabel 'Power [W]';
 xlabel 'Time [s]';
 set(gca, 'FontSize', 20);
 xlim([0 param_simtime]);
-%ylim([-10 10]);
+ylim([-1 10]);
 
 power_ = b1.get('power');
-power_result = 0;
-for i=1:1:size(power_)
-    power_result = power_result + power_(i);
-end
-power_result = power_result / param_simtime;
+%power_result = 0;
+%for i=1:1:size(power_)
+%    power_result = power_result + power_(i);
+%end
+power_result = power_(end) / param_simtime;
 disp('f=34.5 // sawtooth - POWER: ' + string(power_result));
 
 
@@ -147,21 +147,21 @@ xlim([0 param_simtime]);
 ylim([-0.002 0.002]);
 
 figure('Name', 'f=34.5 // sinus - POWER');
-plot(c1.get('time'), c1.get('power'), 'Linewidth', 2, 'DisplayName', 'y_2');
+plot(c1.get('time'), c1.get('power') ./ c1.get('time'), 'Linewidth', 2, 'DisplayName', 'y_2');
 grid;
 grid minor;
 ylabel 'Power [W]';
 xlabel 'Time [s]';
 set(gca, 'FontSize', 20);
 xlim([0 param_simtime]);
-%ylim([-10 10]);
+ylim([-1 10]);
 
 power_ = c1.get('power');
-power_result = 0;
-for i=1:1:size(power_)
-    power_result = power_result + power_(i);
-end
-power_result = power_result / param_simtime;
+%power_result = 0;
+%for i=1:1:size(power_)
+%    power_result = power_result + power_(i);
+%end
+power_result = power_(end) / param_simtime;
 disp('f=34.5 // sinus - POWER: ' + string(power_result));
 
 
@@ -187,21 +187,21 @@ xlim([0 param_simtime]);
 ylim([-0.002 0.002]);
 
 figure('Name', 'f=34.5 // triangle - POWER');
-plot(d1.get('time'), d1.get('power'), 'Linewidth', 2, 'DisplayName', 'y_2');
+plot(d1.get('time'), d1.get('power') ./ d1.get('time'), 'Linewidth', 2, 'DisplayName', 'y_2');
 grid;
 grid minor;
 ylabel 'Power [W]';
 xlabel 'Time [s]';
 set(gca, 'FontSize', 20);
 xlim([0 param_simtime]);
-%ylim([-10 10]);
+ylim([-1 10]);
 
 power_ = d1.get('power');
-power_result = 0;
-for i=1:1:size(power_)
-    power_result = power_result + power_(i);
-end
-power_result = power_result / param_simtime;
+%power_result = 0;
+%for i=1:1:size(power_)
+%    power_result = power_result + power_(i);
+%end
+power_result = power_(end) / param_simtime;
 disp('f=34.5 // triangle - POWER: ' + string(power_result));
 
 
@@ -236,7 +236,20 @@ xlim([0 param_simtime]);
 ylim([-0.002 0.002]);
 legend('show');
 
-
+figure('Name', 'power combined');
+hold on;
+plot(a1.get('time'), a1.get('power') ./ a1.get('time'), 'Linewidth', 2, 'DisplayName', 'Square Wave');
+plot(d1.get('time'), d1.get('power') ./ d1.get('time'), 'Linewidth', 2, 'DisplayName', 'Triangle Wave');
+plot(b1.get('time'), b1.get('power') ./ b1.get('time'), 'Linewidth', 2, 'DisplayName', 'Sawtooth Wave');
+plot(c1.get('time'), c1.get('power') ./ c1.get('time'), 'Linewidth', 2, 'DisplayName', 'Sinus Wave');
+grid;
+grid minor;
+ylabel 'Power [W]';
+xlabel 'Time [s]';
+set(gca, 'FontSize', 20);
+xlim([0 param_simtime]);
+ylim([-1 8]);
+legend('show');
 
 
 
